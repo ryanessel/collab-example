2078aa528b4c6b6e0f52aab6881e7a21014180f0
Ryan Essel <spucti19@gmail.com>
2022-07-09 15:15:51 -0400

    Initial Files Added

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+// alert("Scripts are linked");
+
+// window.onload = () => {
+//     console.log("Scripts are linked")
+// }
+
+// window.addEventListener("laod", () =>{
+//     console.log("Event Listener On load ");
+// })
